3235b4268b52ae4591c16dddac50a38a48bb25f8
Ben Ransford <ransford@cs.umass.edu>
2010-10-14 13:08:34 -0400

    moved handle_* functions into rfid_commands.c

---
+++ rfid_commands.h
@@ -0,0 +1,9 @@
+inline void handle_query (volatile short nextState);
+inline void handle_queryrep (volatile short nextState);
+inline void handle_queryadjust (volatile short nextState);
+inline void handle_select (volatile short nextState);
+inline void handle_ack (volatile short nextState);
+inline void handle_request_rn (volatile short nextState);
+inline void handle_read (volatile short nextState);
+inline void handle_nak (volatile short nextState);
+inline void do_nothing ();
